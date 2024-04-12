1021fcfa853665485a11f79586f3d21253f43fc6
Tomasz Mądry <tke.madry@gmail.com>
2024-04-12 13:27:47 +0200

    initial commit

---
+++ run_snakemake_EXAMPLE_slurm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -i
+
+# remove cluster logs from previous runs so they don't accumulate:
+\mv LyRic/qsub_logs LyRic/qsub_logs_bkp; mkdir LyRic/qsub_logs; rm -rf LyRic/qsub_logs_bkp/ &
+
+# activate snakemake conda environment: 
+conda activate snakemake; 
+
+# launch LyRic workflow in cluster/SLURM mode:
+snakemake -p --reason --latency-wait 100 --use-conda --configfile LyRic/config_EXAMPLE_slurm.json -s LyRic/master.smk -j 500 --jobname {rulename}.{jobid}._{config["PROJECT_NAME"]}_.sh --cluster-config LyRic/cluster_config_slurm.json --max-jobs-per-second 5 --cluster "sbatch --partition={cluster.partition} --tmp={cluster.tmp} --mem={cluster.mem} --time={cluster.time} -o {cluster.out} -e {cluster.err} {cluster.threads}" --rerun-incomplete --keep-going --show-failed-logs
+
+#generate DAG to visualize workflow as SVG image:
+snakemake -p --configfile LyRic/config_EXAMPLE_slurm.json -s LyRic/master.smk --forceall --rulegraph > EXAMPLE_dag.dot
+cat EXAMPLE_dag.dot| dot -Tsvg > EXAMPLE_dag.svg
+
+
+
+
+
